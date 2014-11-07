8163b5916e8a787338f60482c429e97ed11d568c
John Wilson <jwilson@worcester.edu>
2014-11-07 15:44:33 -0500

    Initial check in of application directory.

---
+++ application/classes/Controller/Hello.php
@@ -0,0 +1,14 @@
+<?PHP
+defined('SYSPATH') OR die('No Direct Script Access');
+
+Class Controller_Hello extends Controller_Template
+{
+	public $template = 'site';
+
+	public function action_index()
+	{
+		$this->template->message = 'hellow world';
+	}
+}
+
+?>
